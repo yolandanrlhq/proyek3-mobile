41bb7295709373d0ca033172e312d8fe1c57c4a9
yolandanrlhq <yolandanrlhq@gmail.com>
2026-04-05 15:46:18 +0700

    init flutter project

---
+++ android/app/src/main/kotlin/com/example/app_hara/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_hara
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Login")),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: email,
+              decoration: InputDecoration(labelText: "Email"),
+            ),
+            TextField(
+              controller: password,
+              decoration: InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // sementara langsung masuk
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
+              },
+              child: Text("Login"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
